86e261447f56c5f529004a76ee69a11baeb58898
Mirjan Gjeta <mirjan.gjeta@3i-solutions.net>
2019-01-03 14:41:59 +0100

    First commit

---
+++ src/main/java/com/i3solutions/able2release/Able2ReleaseApplication.java
@@ -0,0 +1,14 @@
+package com.i3solutions.able2release;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Able2ReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Able2ReleaseApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/i3solutions/able2release/Able2ReleaseApplicationTests.java
@@ -0,0 +1,17 @@
+package com.i3solutions.able2release;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Able2ReleaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
